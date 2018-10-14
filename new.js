06fad40df0360e5e7a99a9094207e0c10bcb405e
arfin <md.arfinshekh2017@gmail.com>
2018-10-14 17:31:38 +0600

    sample1

---
+++ new.js
@@ -0,0 +1,14 @@
+
+document.write("Hello World"+"<br/>");
+
+var x=10;
+var y=15;
+var result = x + y;
+document.write("result= "+result+"<br/>");
+
+var str= "Hello World"+"<br/>";
+document.write(str);
+
+
+var multi = x * y;
+console.log("========result========"+multi)
